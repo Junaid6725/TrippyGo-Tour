6d7ed82cbbaadb090aa70ff2a8ea441a8f704052
Junaid Nadeem <junaidnadeem6725@gmail.com>
2025-09-12 15:09:35 +0500

    Delete Outside File of Mono Repo

--- src/pages/adminPages/Users.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { MdDelete } from "react-icons/md";
-import UserDetails from "../../components/admin/adminUsersDetailComponent/UserDetails";
-
-const Users = () => {
-  return <UserDetails />;
-};
-
-export default Users;
