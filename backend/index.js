54244076b28241c58c0daad1020599be244ced4e
Junaid Nadeem <junaidnadeem6725@gmail.com>
2025-11-17 12:57:16 +0500

    Changes in Backend index.js file

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,8 @@ const app = express();
 await connectDB();
 
 const corsOptions = {
-  origin: "https://trippy-go-tour.vercel.app",
+  origin:
+    "https://trippy-go-tour-git-main-muhammad-junaids-projects-36a41707.vercel.app",
   optionsSuccessStatus: 200,
   credentials: true,
 };
